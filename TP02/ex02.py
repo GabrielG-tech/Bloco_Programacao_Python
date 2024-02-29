91a18f479d2f9ffe16340c18343438bb17a9ce84
GabrielG-tech <gabriel@mpsouza.com.br>
2024-02-29 11:56:44 -0300

    ex02 e ex03

--- TP02/ex02.py
+++ TP02/ex02.py
@@ -1,3 +1,14 @@
 # Acumulador de Soma
 # Problema: Desenvolva um programa que use um loop while para somar todos os números até 100 e imprimir o resultado.
 
+# Inicialização das variáveis contador e somatório
+contador = 0  # Inicia o contador em 0
+somatorio = 0  # Inicia o somatório em 0
+
+# Loop while para somar os números até 100
+while contador != 100:  # Enquanto o contador não alcançar 100
+    contador += 1  # Incrementa o contador em 1 a cada iteração
+    somatorio += contador  # Adiciona o valor do contador ao somatório
+
+# Imprime o resultado
+print("A soma dos números de 1 a 100 é", somatorio)
--- TP02/ex03.py
+++ TP02/ex03.py
@@ -1,3 +1,37 @@
 # Concatenação e Organizador de Listas
 # Problema: Crie um programa que concatene duas listas fornecidas pelo usuário e organize a lista em ordem crescente.
 
+def concatenarOrganizar(lista1, lista2):
+    """
+    Concatena duas listas fornecidas e organiza a lista resultante em ordem crescente.
+
+    Args:
+        lista1 (list): A primeira lista a ser concatenada.
+        lista2 (list): A segunda lista a ser concatenada.
+
+    Returns:
+        list: A lista resultante da concatenação e organização das duas listas em ordem crescente.
+    """
+    # Concatena as duas listas
+    lista1.extend(lista2)
+    # Ordena a lista resultante em ordem crescente
+    lista1.sort()
+    return lista1
+
+# Listas fornecidas pelo usuário
+lista1 = []
+lista2 = []
+
+# Loop para inserir elementos na lista1
+for i in range(1, 5):
+    lista1.append(int(input("Digite um número para lista 1: ")))
+    
+# Loop para inserir elementos na lista2
+for i in range(1, 5):
+    lista2.append(int(input("Digite um número para lista 2: ")))
+
+# Chama a função para concatenar e organizar as listas
+resultado = concatenarOrganizar(lista1, lista2)
+
+# Imprime o resultado
+print(resultado)
