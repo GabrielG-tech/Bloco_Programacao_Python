343bb6602a49d36edfebbace3138923817d07910
GabrielG-tech <gabriel@mpsouza.com.br>
2024-03-01 12:43:40 -0300

    ex08 e ex09

--- TP02/ex08.py
+++ TP02/ex08.py
@@ -1,3 +1,27 @@
 # Função de Potência Customizada
 # Problema: Escreva uma função que calcule a potência de um número com um expoente padrão de 2, permitindo ao usuário alterar esse expoente com parâmetros por Keyword e retorne o resultado da operação.
 
+def potencia(base, expoente=2):
+    """
+    Calcula a potência de um número com um expoente padrão de 2.
+
+    Args:
+        base (float): O número base.
+        expoente (float, optional): O expoente para o qual a base será elevada.
+            Padrão é 2.
+
+    Returns:
+        float: O resultado da operação de potência.
+
+    """
+    return base ** expoente
+
+# Testando a função
+base = 3
+expoente = 4
+resultado = potencia(base, expoente)
+print(f"{base} elevado a {expoente} é {resultado}.")
+
+# Testando a função com o expoente padrão
+resultado_padrao = potencia(base)
+print(f"{base} elevado ao expoente padrão é {resultado_padrao}.")
--- TP02/ex09.py
+++ TP02/ex09.py
@@ -1,3 +1,27 @@
 # Função de Fatorial
 # Problema: Escreva uma função que calcule o fatorial de um número passado como argumento e retorne o resultado.
 
+def fatorial(n):
+    """
+    Calcula o fatorial de um número inteiro não negativo.
+
+    Args:
+        n (int): O número para o qual o fatorial será calculado.
+
+    Returns:
+        int: O fatorial de n.
+
+    Raises:
+        ValueError: Se n for um número negativo.
+    """
+    if n < 0:
+        raise ValueError("O fatorial não está definido para números negativos.")
+    if n == 0:
+        return 1
+    else:
+        return n * fatorial(n - 1)
+
+# Testando a função
+numero = int(input("Digite um número para calculo de fatorial: "))
+resultado = fatorial(numero)
+print(f"O fatorial de {numero} é {resultado}.")
