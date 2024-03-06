72a085f3a7cc304c722bc8401d6e704909d8068a
GabrielG-tech <gabriel@mpsouza.com.br>
2024-03-06 12:22:01 -0300

    começo ex11

--- TP02/ex11.py
+++ TP02/ex11.py
@@ -1,3 +1,18 @@
 # Ordenador de Lista por Tamanho de Palavra
 # Problema: Implemente um programa que organize uma lista de palavras em ordem crescente de tamanho.
 
+lista = ["12345", "1234", "1234567", "12345", "12345678", "123456789", "123"]
+lista_ordenada = []
+
+print("Lista original:", lista)
+
+for i in range(len(lista) - 1):
+    for i in range(len(lista) - 1):  # Loop por indice
+        if len(lista[i]) > len(lista[i + 1]):
+            lista_ordenada.append(lista[i + 1])
+            lista_ordenada.append(lista[i])
+    print("Lista filtrada 1ºvez:", lista_ordenada)
+    lista = lista_ordenada
+    lista_ordenada = []
+
+print("Lista filtrada muitas vezes:", lista_ordenada)
