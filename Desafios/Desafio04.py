ba97de9de7ed06d7ac32d525dd3445544302a3c9
GabrielG-tech <gabriel@mpsouza.com.br>
2024-03-07 10:36:37 -0300

    ex11.1

---
+++ Desafios/Desafio04.py
@@ -0,0 +1,4 @@
+# Auto-média ou soma
+# Escreva uma função que receba uma quantidade não definida de valores numéricos e, por padrão, retorne a média entre eles. Ou caso um argumento específico seja passado por uma palavra chave, ela retorne a soma entre estes números
+
+ 
