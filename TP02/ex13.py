d958eed1e1e1c0488101c8706bade6821739f963
GabrielG-tech <gabriel@mpsouza.com.br>
2024-03-07 12:35:31 -0300

    .

--- TP02/ex13.py
+++ TP02/ex13.py
@@ -1,10 +1,10 @@
 # Função Geradora de Histograma
 # Problema: Escreva uma função que receba uma lista de números e imprima um histograma na tela (usando asteriscos para representar a frequência dos números).
 
-lista = list(range(0, 20, 1)) + list(range(20, 0, -1))
+lista = list(range(0, 10, 1)) + list(range(10, 0, -1))
 
 def histograma(lista):
-    largura = 80
+    largura = 60
     for n in range(0, len(lista)//2):
         print("\033[1;44m \033[m"*lista[n] + "\033[1;47m \033[m"*(largura-lista[n]))
     for n in range(len(lista)//2, len(lista)):
