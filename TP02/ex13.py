03cfe56cf66474835ead27b81d6074788499fd80
GabrielG-tech <gabriel@mpsouza.com.br>
2024-03-11 11:36:11 -0300

    ex13 + docString

--- TP02/ex13.py
+++ TP02/ex13.py
@@ -1,11 +1,32 @@
 # Função Geradora de Histograma
 # Problema: Escreva uma função que receba uma lista de números e imprima um histograma na tela (usando asteriscos para representar a frequência dos números).
 
-import random 
+import random
+
 def gerarLista(tamanho, valor_minimo, valor_maximo):
-    return [random.randint(valor_minimo, valor_maximo) for _ in range(tamanho)]
+    """
+    Gera uma lista de números aleatórios.
+
+    Parâmetros:
+    - tamanho: inteiro que representa o tamanho da lista a ser gerada
+    - valor_minimo: inteiro que representa o valor mínimo que pode ser gerado na lista
+    - valor_maximo: inteiro que representa o valor máximo que pode ser gerado na lista
+
+    Retorno:
+    Uma lista de números inteiros aleatórios com o tamanho especificado e dentro do intervalo especificado.
+    """
+    return [random.randint(valor_minimo, valor_maximo) for i in range(tamanho)]
 
 def histograma(lista):
+    """
+    Imprime um histograma na tela usando asteriscos para representar a frequência dos números na lista.
+
+    Parâmetros:
+    - lista: uma lista de números inteiros
+
+    Retorno:
+    A função não retorna nada, apenas imprime o histograma na tela.
+    """
     largura = max(lista)  # Determina a largura máxima do histograma
     for i in range(largura, 0, -1):
         linha = ""
@@ -21,11 +42,10 @@ def histograma(lista):
             print(lista[n], end="   ")
         else:
             print(lista[n], end="  ")
-    
-# Gerar uma lista de números aleatórios
+
+# Exemplo de uso:
 tamanho_da_lista = 10
 valor_minimo = 1
 valor_maximo = 10
 lista = gerarLista(tamanho_da_lista, valor_minimo, valor_maximo)
-
 histograma(lista)
