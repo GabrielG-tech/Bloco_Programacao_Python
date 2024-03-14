2e1b1355c1ce08320db11bcd9c5290d588ea1803
GabrielG-tech <gabriel@mpsouza.com.br>
2024-03-14 10:48:22 -0300

    .

--- Hackathon/NovaTarefa.py
+++ Hackathon/NovaTarefa.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import Interface
+from util import definirTamanhoTela
 
 add_janela = None
 
--- Hackathon/mainTest.py
+++ Hackathon/mainTest.py
@@ -21,16 +21,7 @@ header.pack(fill=X, expand=True, side=TOP, padx=10, pady=10)
 add = Button(header, text="Adicionar tarefa", bg="#6aa84f", bd=0, pady=8, padx=15, fg="white", font=("Arial", 10, "bold"), command=abrir_nova_janela)
 add.pack(side=LEFT)
 
-def definirTamanhoTela(Width = 900, Height = 500):
-    windowWidth = root.winfo_screenwidth()
-    windowHeight = root.winfo_screenheight()
-
-    x = (windowWidth - Width) // 2
-    y = (windowHeight - Height) // 2
-
-    return root.geometry(f"{Width}x{Height}+{x}+{y}")
-
-definirTamanhoTela()
+definirTamanhoTela(root)
 
 # for tarefa in listaTarefas:
 #     print(tarefa)
