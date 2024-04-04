2f1544d8b58baed61c4f3cf29722d170f3c2257f
Gabriel Gomes <gabriel@mpsouza.com.br>
2024-04-03 22:07:54 -0300

    i

--- AT/ex03.py
+++ AT/ex03.py
@@ -1,2 +1,4 @@
 # Número 3: Criar um sistema que simule a interface de um serviço de reservas de ingressos para um cinema, e que permita ao usuário escolher entre os filmes em cartaz no cinema, selecionar entre os assentos disponíveis e fazer a reserva de ingressos.
 
+# (Feito depois de 18h de 03/04/2024)
+
