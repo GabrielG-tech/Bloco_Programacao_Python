26c3cecb7e7f15053834ad83ae94923dc04d9bb2
GabrielG-tech <gabriel@mpsouza.com.br>
2024-06-10 12:27:57 -0300

    .

--- Python para Dados/TP02/ex5.py
+++ Python para Dados/TP02/ex5.py
@@ -9,14 +9,12 @@ arquivo2 = "Python para Dados\\TP02\\ex5_Tab2.xlsx"
 df1 = pd.read_excel(arquivo1)
 df2 = pd.read_excel(arquivo2)
 
-# Combinar os DataFrames
 df_combinados = pd.concat([df1, df2]) # juntar DataFrames
 
-# Calcular o total de vendas para cada produto
+# total de vendas para cada produto
 total_vendas = df_combinados.groupby('Produto')['Vendas'].sum().reset_index()
 
-# Escrever o resultado em um novo arquivo Excel
 resultado = "Python para Dados\\TP02\\ex5_resultado.xlsx"
-total_vendas.to_excel(resultado, index=False)
+total_vendas.to_excel(resultado, sheet_name='Total Vendas',index=False)
 
 print("Dados combinados e total de vendas calculado com sucesso! Resultado salvo em", resultado)
--- Python para Dados/TP02/ex6.py
+++ Python para Dados/TP02/ex6.py
@@ -1,2 +1,26 @@
 # Dado um DataFrame contendo informações sobre filmes (titulo, gênero, ano de lançamento), converta-o para o formato JSON. Em seguida, carregue esse JSON de volta para um DataFrame e adicione uma nova coluna com a duração dos filmes. Salve o DataFrame final como um novo arquivo JSON.
 
+import pandas as pd
+import json
+
+data = {
+    'titulo': ['Filme 1', 'Filme 2', 'Filme 3'],
+    'genero': ['Ação', 'Comédia', 'Drama'],
+    'ano_lancamento': [2020, 2019, 2018]
+}
+df = pd.DataFrame(data)
+
+# Converte o DataFrame para JSON:
+df_json = df.to_json(orient='records', indent=4) # lista de dicionários
+
+# Escrever JSON:
+with open('Python para Dados\\TP02\\ex6.json', 'w', encoding='utf-8') as f:
+    f.write(df_json)
+
+# Carrega JSON:
+with open('Python para Dados\\TP02\\ex6.json', 'r', encoding='utf-8') as f:
+    df_loaded = pd.read_json(f)
+
+df_loaded['duracao'] = [120, 90, 110]
+
+df_loaded.to_json('Python para Dados\\TP02\\ex6_filmes.json', orient='records', indent=4)
