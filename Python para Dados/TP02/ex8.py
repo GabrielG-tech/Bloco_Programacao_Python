d93cf8f0cdff33fb8d96b8069a0e73e3671feb2a
GabrielG-tech <gabriel@mpsouza.com.br>
2024-06-12 11:25:08 -0300

    TP02 Terminado

--- Python para Dados/TP02/ex8.py
+++ Python para Dados/TP02/ex8.py
@@ -1,40 +1,61 @@
 # Baixe o código do link e execute ele para obter um arquivo fictício de log. Escreva um script que processe esse arquivo de log gerado, tratando-o como um arquivo grande. Crie uma função que filtre as mensagens de erro especificadas no log e salve o timestamp dos erros em um novo arquivo para análise posterior. Crie uma outra função que explore o arquivo log e crie uma lista dos IPs que logaram no sistema e salve em um arquivo ordenado do IP que mais acessou para o IP que menos acessou, registrando a quantidade de loggins logins realizados pelo IP.
 
 import re
-from collections import Counter
 
-# Função para filtrar mensagens de erro e salvar timestamps
-def filtrar_erros(logfile, erro_file):
-    with open(logfile, 'r') as f, open(erro_file, 'w') as ef:
-        for line in f:
-            if 'ERROR' in line:
-                # Extraindo o timestamp usando regex
-                timestamp = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', line).group()
-                ef.write(timestamp + '\n')
-
-# Função para listar IPs e contar logins
-def listar_ips(logfile, ip_file):
-    ip_counter = Counter()
-    with open(logfile, 'r') as f:
-        for line in f:
-            # Extraindo o IP usando regex
-            match = re.search(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', line)
-            if match:
-                ip = match.group()
-                ip_counter[ip] += 1
+def filtrar_erros(arquivo_log, arquivo_saida_erros):
+    """
+    Filtra mensagens de erro no arquivo de log e salva os timestamps dos erros em um novo arquivo.
+    """
+    arquivo_log = open(arquivo_log, 'r')
+    arquivo_erros = open(arquivo_saida_erros, 'w')
     
-    # Ordenar IPs pela quantidade de logins (descendente)
-    sorted_ips = sorted(ip_counter.items(), key=lambda x: x[1], reverse=True)
+    for linha in arquivo_log:
+        if "ERROR" in linha:
+            timestamp = linha.split(' ')[0]  # Supondo que o timestamp está no início da linha
+            arquivo_erros.write(timestamp + '\n')
     
-    with open(ip_file, 'w') as f:
-        for ip, count in sorted_ips:
-            f.write(f'{ip} {count}\n')
+    arquivo_log.close()
+    arquivo_erros.close()
 
-# Arquivos de entrada e saída
-logfile = 'Python para Dados\\TP02\\sample_log_file.log'
-erro_file = 'Python para Dados\\TP02\\ex8_erro_timestamps.txt'
-ip_file = 'Python para Dados\\TP02\\ex8_ips_logins.txt'
+def listar_ips(arquivo_log, arquivo_saida_ips):
+    """
+    Lista os IPs que logaram no sistema e salva em um arquivo ordenado do IP que mais acessou para o IP que menos acessou.
+    """
+    contagem_ips = {}
+    padrao_ip = re.compile(r'(\d{1,3}\.){3}\d{1,3}')
+    
+    arquivo_log = open(arquivo_log, 'r')
+    for linha in arquivo_log:
+        ip_encontrado = padrao_ip.search(linha)
+        if ip_encontrado:
+            ip = ip_encontrado.group(0)
+            if ip in contagem_ips:
+                contagem_ips[ip] += 1
+            else:
+                contagem_ips[ip] = 1
+    arquivo_log.close()
 
-# Executar as funções
-filtrar_erros(logfile, erro_file)
-listar_ips(logfile, ip_file)
+    ips_ordenados = ordenar_por_valor(contagem_ips)
+
+    arquivo_ips = open(arquivo_saida_ips, 'w')
+    for ip, contagem in ips_ordenados:
+        arquivo_ips.write(f"{ip} {contagem}\n")
+    arquivo_ips.close()
+
+def ordenar_por_valor(dicionario):
+    """
+    Ordena um dicionário por seus valores em ordem decrescente.
+    """
+    itens = list(dicionario.items())
+    for i in range(len(itens)):
+        for j in range(i + 1, len(itens)):
+            if itens[i][1] < itens[j][1]:
+                itens[i], itens[j] = itens[j], itens[i]
+    return itens
+
+arquivo_log = 'Python para Dados/TP02/sample_log_file.log'
+arquivo_erros = 'Python para Dados/TP02/ex8_erro_timestamps.txt'
+arquivo_ips = 'Python para Dados/TP02/ex8_ips_logins.txt'
+
+filtrar_erros(arquivo_log, arquivo_erros)
+listar_ips(arquivo_log, arquivo_ips)
--- Python para Dados/TP02/ex9.py
+++ Python para Dados/TP02/ex9.py
@@ -1,2 +1,17 @@
 # Escreva uma função que solicita ao usuário dois números e tenta dividir o primeiro pelo segundo. Use o tratamento de exceções para lidar com a entrada inválida (ex: texto ao invés de número) e a divisão por zero, informando ao usuário a natureza do erro.
 
+def dividir_numeros():
+    while True:
+        try:
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+            resultado = num1 / num2
+        except ValueError:
+            print("Erro: Por favor, digite valores numéricos válidos.")
+        except ZeroDivisionError:
+            print("Erro: Divisão por zero não é permitida.")
+        else:
+            print(f"O resultado da divisão é: {resultado}")
+            break
+        
+dividir_numeros()
