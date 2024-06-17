16351df29222d78e462ed9ef557d5b4379f04108
Gabriel Gomes <gabriel@mpsouza.com.br>
2024-06-17 14:12:42 -0300

    .

--- Python para Dados/TP03/Parte 1/ex02.py
+++ Python para Dados/TP03/Parte 1/ex02.py
@@ -3,12 +3,9 @@
 import pandas as pd
 import sqlite3
 
-<<<<<<< HEAD
 PATH = 'Python para Dados\\TP03\\Parte 1\\'
 DATABASE_PATH = PATH + 'ex02_pessoas.db'
 
-=======
->>>>>>> 3b10b5f40c11c93c3ef70382dba06133ad908644
 data = {
     'nome': ['Ana', 'Carlos'],
     'idade': [25, 30],
@@ -16,17 +13,10 @@ data = {
 }
 df = pd.DataFrame(data)
 
-<<<<<<< HEAD
 # Conecta no banco de dados SQLite (ou criar se não existir)
 conn = sqlite3.connect(DATABASE_PATH)
 
 # Armazena o DataFrame na tabela pessoas
-=======
-# Conecta SQLite DB (ou criar se não existir)
-conn = sqlite3.connect('ex02_pessoas.db')
-
-# Armazena o DF na tabela "pessoas"
->>>>>>> 3b10b5f40c11c93c3ef70382dba06133ad908644
 df.to_sql('pessoas', conn, if_exists='replace', index=False)
 conn.close()
 
